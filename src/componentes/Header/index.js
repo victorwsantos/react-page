0e298ae147860d2dbdecb827bc94e0facad94053
victorwsantos <71085593+victorwsantos@users.noreply.github.com>
2021-09-08 00:19:28 -0300

    NavButton pronto

--- src/componentes/Header/index.js
+++ src/componentes/Header/index.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 export default function Header(){
+    const btn = document.getElementById('btn-mb')
+    function toggleMenu(){
+        const nav = document.getElementById('navega');
+        nav.classList.toggle('active');
+
+    }
     return(
         <div className='topo'>
             <div className='componentes-topo'>
                 <p className='logo'>Victor Santos</p>
-                <nav>
-                    <ul>
+                <nav id='navega'>
+                    <ul id='menu'>
                         <li><Link className="Home" to='/'>Home</Link></li>
                         <li><Link className="objetivos" to='/Objetivos'>Objetivos</Link></li>
                         <li><Link className="sobre" to='/Sobre'>Sobre</Link></li>
                         <li><Link className="Contatos" to='/Contatos'>Contatos</Link></li>
                     </ul>
                 </nav>
+                <button id='btn-mb' className="navbutton" onClick={toggleMenu}>&#9776;</button>
             </div>
        </div>) 
 }
