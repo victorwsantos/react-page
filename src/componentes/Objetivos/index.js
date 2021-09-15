268615875eae0070f22baa6859036f18f521bffd
victorwsantos <71085593+victorwsantos@users.noreply.github.com>
2021-09-15 16:06:55 -0300

    Construção página objetivos

--- src/componentes/Objetivos/index.js
+++ src/componentes/Objetivos/index.js
@@ -5,10 +5,21 @@ import '../../App.css';
 const Objetivos = () => (
     <div>
     < Header />
-    <div>
-        <p>
-            Objetivos
-        </p>
+    <div id='paginaObjetivos'>
+        <div id='objetivos'>
+            <div id="apresentacao">
+                <h2>Ser um Desenvolvedor completo</h2>
+                <p>Certo dia vi esta frase na parede da empresa em que trabalho e  isto me fez refletir principamente sobre a minha carreira. Aplicar esta metodologia de que tudo pode ser melhorado
+                    acaba sendo de certa forma um mantra no ramo do desenvolvimento de software, onde devemos melhora-los dia após dia!
+                </p>
+                </div>
+                <div>
+                    img
+                </div>
+            </div>
+            <div className='sonhos'>
+                Sou extremamente motivado por sonhos e conquistas
+            </div>
     </div>
     < Footer  />
 
