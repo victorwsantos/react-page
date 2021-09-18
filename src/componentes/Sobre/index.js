7421ac52899465863ad680a39028958f6c8a4191
Victor Santos <71085593+victorwsantos@users.noreply.github.com>
2021-09-17 23:46:23 -0300

    Start changes

--- src/componentes/Sobre/index.js
+++ src/componentes/Sobre/index.js
@@ -2,13 +2,15 @@ import React from "react";
 import Header from "../Header"
 import Footer from "../Footer"
 import '../../App.css';
+import Card from "../Cards";
 const Sobre = () => (
     <div>
     < Header />
     <div>
-        <p>
-            Sobre
-        </p>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
     </div>
     < Footer  />
 
