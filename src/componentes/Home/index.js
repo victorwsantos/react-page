2fc479770a44d51a85aad089a7505f5dd976c623
Victor Santos <71085593+victorwsantos@users.noreply.github.com>
2021-09-26 01:46:23 -0300

    upload photo

--- src/componentes/Home/index.js
+++ src/componentes/Home/index.js
@@ -9,16 +9,21 @@ function Home() {
   return (
     <div className='pg-completa'>
       <Header />
+            <div id='topPage'>
+                  <div id='imgIndex-phone'>
+                      <img width='290px' height='250px' src='imagens/imagem-3.png'/>
+                  </div>
               <div id="apresentacao">
-                  <h2>Ser um Desenvolvedor completo</h2>
+                  <h2>Seja muito bem vindo!!</h2>
                   <p><b><span>"</span>Everything can be improved."</b></p>
                   <p>Certo dia vi esta frase na parede da empresa em que trabalho e  isto me fez refletir principamente sobre a minha carreira. Aplicar esta metodologia de que tudo pode ser melhorado
                       acaba sendo de certa forma um mantra no ramo do desenvolvimento de software, onde devemos melhora-los dia após dia!
                   </p>
               </div>
-                  <div>
-                      img
+                  <div id='imgIndex'>
+                      <img width='490px' height='500px' src='imagens/imagem-3.png'/>
                   </div>
+              </div>
               <div className='sonhos'>
                   <p>Este Portfólio tem como principal objetivo apresentar minhas skills na cricação de Web Sites utilizando variadas técnologias.</p>
               </div>
