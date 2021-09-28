26c3fbca99152ad95a1e3215643913843498a2f3
victorwsantos <71085593+victorwsantos@users.noreply.github.com>
2021-09-28 11:08:43 -0300

    Acertos de versão

--- src/componentes/Cards/index.js
+++ src/componentes/Cards/index.js
@@ -4,28 +4,27 @@ import Linkedin from '../Linkedin'
 import Whats from '../Whats'
 export default function Cards(){
     function go(){
-        const goto = document.getElementById('one')
         const what = document.getElementById('Whats')
         const edin = document.getElementById('linkeDin')
         const disc = document.getElementById('Discord')
-        edin.style.display=('block')
-        what.style.display=('none')
+        what.style.display=('block')
+        disc.style.display=('none')
+        edin.style.display=('none')
 
     }
     function gotow(){
         
         const edin = document.getElementById('linkeDin')
-        const goto = document.getElementById('tow')
-        const edin = document.getElementById('linkeDin')
         const what = document.getElementById('Whats')
         const disc = document.getElementById('Discord')
-        what.style.display=('block')
-        edin.style.display=('none')
+        what.style.display=('none')
+        disc.style.display=('none')
+        edin.style.display=('block')
 
     }
 
     function gothree(){
-        const goto = document.getElementById('three')
+        
         const edin = document.getElementById('linkeDin')
         const what = document.getElementById('Whats')
         const disc = document.getElementById('Discord')
@@ -33,15 +32,7 @@ export default function Cards(){
         edin.style.display=('none')
         disc.style.display=('block')
     }
-    function goout(){
-        const goto = document.getElementById('three')
-        const edin = document.getElementById('linkeDin')
-        const what = document.getElementById('Whats')
-        const disc = document.getElementById('Discord') 
-        what.style.display=('none')
-        edin.style.display=('none')
-        disc.style.display=('none')
-    }
+    
     return(
         
         <div  className='div-contatos'>
@@ -49,9 +40,11 @@ export default function Cards(){
                 <h1>Gostou do meu conteudo?</h1>
                     <p>Não perca tempo, entre em em contato!!!</p>
                 <div className='botoes-pg-index'>
-
+                    <button onClick={go}>WhatsApp</button>
+                    <button onClick={gotow}>Linkedin</button>
+                    <button onClick={gothree}>GitHub</button>
                 </div>
-                <div id='Whats'>
+            <div id='Whats'>
             < Whats/>
             </div>
             <div id='linkeDin'>
