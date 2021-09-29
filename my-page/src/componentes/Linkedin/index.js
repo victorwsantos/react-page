313b2202bff532dec81587fa82f10b687852ee12
Victor Santos <71085593+victorwsantos@users.noreply.github.com>
2021-09-28 21:35:03 -0300

    new Build

--- my-page/src/componentes/Linkedin/index.js
+++ my-page/src/componentes/Linkedin/index.js
@@ -4,6 +4,7 @@ import Box from '../Box';
 export default function linkedin(){
     return(
         <div className='linke'>
+            < Box titulo='Victor W Santos.'  informacao='Vamos nos conectar?'/>
             <a href='https://www.linkedin.com/in/victor-w-santos-039bba123/' target='_blank'><button>Fazer Conexão</button></a>
         </div>
     )
