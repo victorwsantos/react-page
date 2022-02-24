4e5e101607e52513cf3f11c6f1ab365c3f10bade
victor.cassiano <victor.cassiano@g.globo>
2022-02-23 22:39:07 -0300

    Feito

--- src/componentes/Body/index.js
+++
@@ -1,9 +0,0 @@
-import React from  'react';
-import Card from '../Cards';
-export default function Body(){
-    return(
-        <div>
-            <Card />
-        </div>
-    )
-}
--- src/componentes/Footer/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-export default function Footer(){
-    return(
-        <div className='conteudo-footer'>
-            <footer>Está é uma página construida em React</footer>
-        </div>
-    )
-}
